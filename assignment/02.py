afd5c753c71ad1eab61c79e2d255964736ba8e44
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-13 16:05:55 +0900

    fix: 수정된 풀이

--- assignment/02.py
+++ assignment/02.py
@@ -9,10 +9,15 @@ k = 1.381e-23  # 볼츠만 상수 (J/K)
 I_D = 0.1e-3  # 다이오드 전류 (암페어)
 I_S = 1e-15  # 포화 전류 (암페어)
 KELVIN_CONVERSION = 273.15  # 켈빈 변환 상수
+C_1 = I_S / np.exp(-E_g/(300*k)) # C_1 상수
+print("C1 : ", C_1)
+x = 0.0259 * np.log(I_D / I_S)
+# x를 출력
+print("x : ", x);
 
 # Va를 계산하는 함수
 def calculate_V_a(T_kelvin, I_D, I_S):
-    return (E_g / e) - ((k * T_kelvin) / e) * np.log(I_S / I_D)
+    return (E_g / e) - ((k * T_kelvin) / e) * np.log(C_1 / I_D)
 
 # 온도 범위 정의 (20°C to 200°C), 켈빈으로 변환
 T_min = 20 + KELVIN_CONVERSION
@@ -41,4 +46,6 @@ V_a_20C = calculate_V_a(20+KELVIN_CONVERSION, I_D, I_S)
 V_a_200C = calculate_V_a(200+KELVIN_CONVERSION, I_D, I_S)
 print(f'V_a at T=20C: {V_a_20C:.3f} Volts')
 print(f'V_a at T=200C: {V_a_200C:.3f} Volts')
+
+
 
